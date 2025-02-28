06554713b6fa91512334b5e963b4636dedfa2d4c
stephenqiu <Popcornqhd@gmail.com>
2025-02-28 15:56:23 +0800

    feat: 添加科大讯飞和deepseek的大模型ai支持

---
+++ src/main/java/com/stephen/interview/config/ai/deepseek/condition/DeepSeekAICondition.java
@@ -0,0 +1,21 @@
+package com.stephen.interview.config.ai.deepseek.condition;
+
+import org.apache.commons.lang3.StringUtils;
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+/**
+ * 是否开启DeepSeekAI
+ *
+ * @author: stephen qiu
+ **/
+public class DeepSeekAICondition implements Condition {
+	
+	@Override
+	public boolean matches(ConditionContext context, @NotNull AnnotatedTypeMetadata metadata) {
+		String property = context.getEnvironment().getProperty("deepseek.enabled");
+		return StringUtils.equals(Boolean.TRUE.toString(), property);
+	}
+}
